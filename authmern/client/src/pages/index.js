3978b6f72634c06644380cb1529c50e90b1da582
VAANI BANSAL <140154294+dinosaur21@users.noreply.github.com>
2024-11-07 13:52:56 +0530

    Update index.js

--- authmern/client/src/pages/index.js
+++ authmern/client/src/pages/index.js
@@ -4,4 +4,10 @@ export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
 export { default as Logout } from "./Logout";
-export {default as Qrscanner} from "./Qrscanner"
+export {default as Qrscanner} from "./Qrscanner";
+export {default as ManufacturerRegister} from "./ManufacturerRegister";
+export {default as ManufacturerLogin} from "./ManufacturerLogin";
+export {default as RALogin} from "./RegulatoryLogin";
+export {default as RARegister} from "./RegulatoryRegister";
+export {default as RADashboard} from "./RegulatoryDashboard";
+
