59ba40639d02532c060d7c06856fd9091d653138
VAANI BANSAL <140154294+dinosaur21@users.noreply.github.com>
2024-11-07 13:50:46 +0530

    Update ManufacturerLogin.jsx

--- authmern/client/src/pages/ManufacturerLogin.jsx
+++ authmern/client/src/pages/ManufacturerLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './ManufacturerLogin.css';
+import '../styles/ManufacturerLogin.css';
 
 const ManufacturerLogin = () => {
   const [formData, setFormData] = useState({
@@ -26,6 +26,7 @@ const ManufacturerLogin = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <h1>MANUFACTURER LOGIN</h1>
       <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
       <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
       <button type="submit">Login</button>
